ad4ca57b26c11868bf006fdd754cb4bdadd07373
David Vaquero <pepesan@gmail.com>
2022-07-10 20:28:51 +0200

    commit inicial

---
+++ src/main/java/com/cursosdedesarrollo/springbootremoting/SpringBootRemotingApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.springbootremoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRemotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRemotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursosdedesarrollo/springbootremoting/SpringBootRemotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.springbootremoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRemotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
